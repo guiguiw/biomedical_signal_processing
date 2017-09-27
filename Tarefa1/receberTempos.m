e8774d55e0db3c6505b599f937a835a1c7d49ccd
Italo Fernandes on Ubuntu-Inspiron <italogsfernandes@gmail.com>
2017-09-27 20:13:37 -0300

    recebe o tempo de inicio e tempo de final

---
+++ Tarefa1/receberTempos.m
@@ -0,0 +1,7 @@
+function [ t_start, t_end ] = receberTempos( )
+%RECEBERTEMPOS Recebe do usario o tempo de inicio e tempo de final
+%   Detailed explanation goes here
+    t_start = input ('Digite o tempo de inicio [s]: '); 
+    t_end = input ('Digite o tempo final [s]: ');
+end
+
