c24b425d2b6ed07415d76149f16e69bced216b11
Ítalo Fernandes <italogsfernandes@gmail.com>
2018-07-12 15:33:30 -0300

    correcao da integral_m finalizada

--- Tarefas_Disciplina_MTS/Tarefa_4_-_Estimulo_auditivo/corrigindo_a_integral.m
+++ Tarefas_Disciplina_MTS/Tarefa_4_-_Estimulo_auditivo/corrigindo_a_integral.m
@@ -24,9 +24,22 @@ eeg_powers(index_gama) = 2;
 eeg_powers(index_s_gama) = 1;
 
 %% 
+figure();
 plot(eeg_freqs, eeg_powers)
 grid on;
+ylim([0, 7]);
 title('Sinal simulado');
+xlabel('Frequência (Hz)');
+ylabel('Valores simulados');
+
+%% adicionando um pouquinho de ruido
+eeg_powers = eeg_powers + 0.1 * randn(1,length(eeg_powers));
+%% 
+figure();
+plot(eeg_freqs, eeg_powers)
+grid on;
+ylim([0, 7]);
+title('Sinal simulado com ruído simulado');
 xlabel('Frequência (Hz)');
 ylabel('Valores simulados');
 
@@ -39,6 +52,7 @@ areas_simuladas = valores_base_x .* valor_altura_y;
 
 
 %% Plotando valores reais
+figure();
 bar(areas_simuladas);
 title("Áreas Simuladas");
 ylim([0, 120]);
@@ -66,6 +80,7 @@ s_gama_pwr = df*trapz(eeg_powers(eeg_freqs >= 80.0 & eeg_freqs < 100.0));
 areas_trapz = [delta_pwr,teta_pwr, alpha_pwr, beta_pwr, gama_pwr, s_gama_pwr];
 
 %% Plotando valores trapz
+figure();
 bar(areas_trapz);
 title("Áreas pela função trapz");
 ylim([0, 120]);
@@ -81,39 +96,71 @@ for ii=1:6
 end
 
 %% integral.m
-df = eeg_freqs(2) - eeg_freqs(1);
-delta_pwr =  df*trapz(eeg_powers(eeg_freqs >=    0 & eeg_freqs <   3.5));
-teta_pwr =   df*trapz(eeg_powers(eeg_freqs >=  3.5 & eeg_freqs <   7.5));
-alpha_pwr =  df*trapz(eeg_powers(eeg_freqs >=  7.5 & eeg_freqs <  12.5));
-beta_pwr =   df*trapz(eeg_powers(eeg_freqs >= 12.5 & eeg_freqs <  30.0));
-gama_pwr =   df*trapz(eeg_powers(eeg_freqs >= 30.0 & eeg_freqs <  80.0));
-s_gama_pwr = df*trapz(eeg_powers(eeg_freqs >= 80.0 & eeg_freqs < 100.0));
+delta_pwr =  integral_antiga(eeg_powers(eeg_freqs >=    0 & eeg_freqs <   3.5));
+teta_pwr =   integral_antiga(eeg_powers(eeg_freqs >=  3.5 & eeg_freqs <   7.5));
+alpha_pwr =  integral_antiga(eeg_powers(eeg_freqs >=  7.5 & eeg_freqs <  12.5));
+beta_pwr =   integral_antiga(eeg_powers(eeg_freqs >= 12.5 & eeg_freqs <  30.0));
+gama_pwr =   integral_antiga(eeg_powers(eeg_freqs >= 30.0 & eeg_freqs <  80.0));
+s_gama_pwr = integral_antiga(eeg_powers(eeg_freqs >= 80.0 & eeg_freqs < 100.0));
 
-areas_trapz = [delta_pwr,teta_pwr, alpha_pwr, beta_pwr, gama_pwr, s_gama_pwr];
-
+areas_integral_m = [delta_pwr,teta_pwr, alpha_pwr, beta_pwr, gama_pwr, s_gama_pwr];
+areas_integral_m = abs(areas_integral_m);
 %% plotando integral.m
-bar(areas_trapz);
-title("Áreas pela função trapz");
+figure();
+bar(areas_integral_m);
+title("Áreas pela função integral.m");
 ylim([0, 120]);
 ylabel("Áreas");
 xlabel("Bandas de Frequência");
 grid on;
 set(gca,'XTickLabel', {'delta', 'teta', 'alpha', 'beta', 'gama', 's gama'});
 for ii=1:6
-    conta_em_str = num2str(areas_trapz(ii),'%0.1f');
-    text(ii,areas_trapz(ii), conta_em_str,...
+    conta_em_str = num2str(areas_integral_m(ii),'%0.1f');
+    text(ii,areas_integral_m(ii), conta_em_str,...
+               'HorizontalAlignment','center',...
+               'VerticalAlignment','bottom')
+end
+
+%% integral.m corrigida
+delta_pwr =  integral(eeg_powers(eeg_freqs >=    0 & eeg_freqs <   3.5), df);
+teta_pwr =   integral(eeg_powers(eeg_freqs >=  3.5 & eeg_freqs <   7.5), df);
+alpha_pwr =  integral(eeg_powers(eeg_freqs >=  7.5 & eeg_freqs <  12.5), df);
+beta_pwr =   integral(eeg_powers(eeg_freqs >= 12.5 & eeg_freqs <  30.0), df);
+gama_pwr =   integral(eeg_powers(eeg_freqs >= 30.0 & eeg_freqs <  80.0), df);
+s_gama_pwr = integral(eeg_powers(eeg_freqs >= 80.0 & eeg_freqs < 100.0), df);
+
+areas_integral_corrigida = [delta_pwr,teta_pwr, alpha_pwr, beta_pwr, gama_pwr, s_gama_pwr];
+%% plotando integral.m corrigida
+figure();
+bar(areas_integral_corrigida);
+title("Áreas pela função integral.m corrigida");
+ylim([0, 120]);
+ylabel("Áreas");
+xlabel("Bandas de Frequência");
+grid on;
+set(gca,'XTickLabel', {'delta', 'teta', 'alpha', 'beta', 'gama', 's gama'});
+for ii=1:6
+    conta_em_str = num2str(areas_integral_corrigida(ii),'%0.1f');
+    text(ii,areas_integral_corrigida(ii), conta_em_str,...
                'HorizontalAlignment','center',...
                'VerticalAlignment','bottom')
 end
 
 %% Plotando
-todos_valores = [rest stim];
-bar(rest_stim);
+figure();
+todos_valores = [areas_trapz' areas_simuladas' areas_integral_m' areas_integral_corrigida'];
+bar(todos_valores);
 
+ylim([0, 120]);
 title("Áreas Caluladas pelos diferentes métodos");
-legend('Real', 'Trapz', 'integral.m');
+legend('Trapz', 'Simulada', 'integral.m', 'integral.m corrigida');
 ylabel("Áreas");
 xlabel("Bandas de Frequência");
 grid on;
-
 set(gca,'XTickLabel', {'delta', 'teta', 'alpha', 'beta', 'gama', 's gama'});
+for ii=1:6
+    conta_em_str = num2str(areas_simuladas(ii),'%0.1f');
+    text(ii,areas_simuladas(ii), conta_em_str,...
+               'HorizontalAlignment','center',...
+               'VerticalAlignment','bottom')
+end
